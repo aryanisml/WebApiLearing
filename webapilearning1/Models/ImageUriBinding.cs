20c787f819e1fde22beecc40f54cfc0b709bf276
swapnil.kamble02@gmail.com <swapnil.kamble02@gmail.com>
2016-03-15 02:31:48 +0530

    model binding in Web Api

---
+++ webapilearning1/Models/ImageUriBinding.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPILearning1.Models
+{
+    public class ImageUriBinding
+    {
+        public int imageId { get; set; }
+        public string imageName { get; set; }
+    }
+}
